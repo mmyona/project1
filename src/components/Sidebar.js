0c618556ecf7f6eaffe07050e3c056b5e5b88346
임현아 <90781929+mmyona@users.noreply.github.com>
2023-01-07 11:19:09 +0900

    [feat] 뼈대 구축 (#1)

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import "../css/sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="content">
+      <div></div>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/pages/Mainpage.js
@@ -0,0 +1,11 @@
+import "../css/mainpage.css";
+
+function Mainpage() {
+  return (
+    <div className="content">
+      <div></div>
+    </div>
+  );
+}
+
+export default Mainpage;
