29bb87233924ae10af3cb13d2e0fc8d061573149
mmyona <hyon2001@ajou.ac.kr>
2023-03-23 23:49:34 +0900

    feat : theme color 추가

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import { Router } from "./Router/Router";
+import theme from "./components/theme";
+import { ThemeProvider } from "styled-components";
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <Router />
-    </div>
+    </ThemeProvider>
   );
 }
 
-/*
-
-      <div>
-        <p>
-          <Link to="/">HOME</Link>
-        </p>
-        <p>
-          <Link to="/r1">Router1</Link>
-        </p>
-        <p>
-          <Link to="/r2">Unity map</Link>
-        </p>
-      </div>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/r1" element={<Router1 />} />
-          <Route path="/r2" element={<Router2 />} />
-        </Routes>
-      </div>
-      */
 export default App;
---
+++ src/components/theme.js
@@ -0,0 +1,10 @@
+const color = {
+  black: "#000000",
+  gra_red: "#801304",
+  navy: "#050D3A",
+  gray: "#E5E5E5",
+};
+
+const theme = { color };
+
+export default theme;
