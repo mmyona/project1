0a1d19f6322b6ecf145ba08caf6951fc7403383c
mmyona <hyon2001@ajou.ac.kr>
2023-05-04 12:48:31 +0900

    #16 feat : 고객 센터 뼈대 구축, 하위 메뉴 컴포넌트 추가

--- src/pages/Center.js
+++ src/pages/Center.js
@@ -1,7 +1,37 @@
 import React from "react";
 import styled from "styled-components";
 import theme from "../components/theme";
+import Nav from "react-bootstrap/Nav";
+import { Btn } from "../components/atoms/Button";
 
 export const Center = () => {
-  return <></>;
+  return (
+    <CenterContainer>
+      <Nav fill variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/center">자주 하는 질문</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Q&A</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">전시 의뢰</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-3">버그 제보</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </CenterContainer>
+  );
 };
+
+const CenterContainer = styled.div`
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  max-width: 2000px; /* Nav 컴포넌트의 최대 너비 */
+  margin: 0 auto;
+`;
