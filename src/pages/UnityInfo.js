8eb3ec9261739bd9ceb36d9475dc3bcb0967b0ee
mmyona <hyon2001@ajou.ac.kr>
2023-05-30 17:05:01 +0900

    feat : dummy 데이터 추가

--- src/pages/UnityInfo.js
+++ src/pages/UnityInfo.js
@@ -5,14 +5,25 @@ import { Btn } from "../components/atoms/Button";
 import { Link } from "react-router-dom";
 import { Content } from "../components/Content";
 import { useParams } from "react-router-dom";
+import { UnityListData } from "../dummy/UnityListData";
 
 export const UnityInfo = () => {
   const { unityId } = useParams();
+  const userId = localStorage.getItem("userId");
+  const unityMap = UnityListData[unityId - 1];
   return (
     <InfoContainer>
-      <h2>Title</h2>
+      <h1>{unityMap.title}</h1>
+      <FlexContainer>
+        <img alt="썸네일 이미지" src={unityMap.src} />
+        <div>
+          <h3>맵 설명</h3>
+          <p>{unityMap.text}</p>
+        </div>
+      </FlexContainer>
       <Content />
-      <Link to={`/unity${unityId}`}>
+      <p>전시에 입장하려면 밑의 버튼을 누르세요</p>
+      <Link to={`/unity${unityId}?userid=${userId}`}>
         <Btn
           btnText="Enter"
           width="4rem"
@@ -20,6 +31,7 @@ export const UnityInfo = () => {
           fontFamily="Bebas Neue"
           fontsize="1.4rem"
           color={theme.color.gray}
+          margin="0 0 0 1rem"
         />
       </Link>
     </InfoContainer>
@@ -32,7 +44,29 @@ const InfoContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  h2 {
+  h1 {
     font-family: "Bebas Neue", sans-serif;
+  }
+  h3 {
+    line-height: 3rem;
+  }
+  p {
+    line-height: 2rem;
+  }
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 1rem;
+  img {
+    height: 27rem;
+  }
+  div {
+    padding: 1rem;
+    p {
+      white-space: pre-wrap;
+    }
   }
 `;
--- src/pages/UnityList.js
+++ src/pages/UnityList.js
@@ -4,45 +4,13 @@ import styled from "styled-components";
 
 //components
 import { UnityItem } from "../components/UnityItem";
+import { UnityListData } from "../dummy/UnityListData";
 
 export const UnityList = () => {
-  const data = [
-    {
-      id: 1,
-      title: "Chess",
-      text: " 남녀노소 누구나 즐길 수 있는 보드게임인 체스를 컨셉으로 한 3D맵, 체스판이라는 무대 위에서 움직이는 것이 PLAYER의 머릿속에서 움직인다는 느낌을 준다.",
-      src: "images/chess.png",
-    },
-    {
-      id: 2,
-      title: "Park",
-      text: "현실에서는 여러모로 전시가 힘든 공간이 있다. 대부분의 전시가 실내에서 이루어지는 이유가 바로 그것인데, 이를 기반으로 야외에서의 무대를 제작하기로 하였다.자연경관과 비슷한 느낌을 주는 공원, 특히 연못 위에서의 전시는 실현이 어렵기 때문에 공원이라는 컨셉의 3D맵은 보통의 전시에서는 느낄 수 없는 개방감을 선사해줄것이라 기대된다.",
-      src: "images/park.png",
-    },
-    {
-      id: 3,
-      title: "Plaza",
-      text: " 위 두 맵과는 반대로 현실과 가장 비슷한 3D맵도 제작하였다. 직접 전시에 가지 않아도 된다는 느낌을 살리기 위해 실제 전시회를 참고하여 만들었다.맵에 놓인 주사위, 볼펜, 맵 중앙의 나무 등 여러모로 잡동사니를 두어 심심한 공간을 채워주었다.",
-      src: "images/plaza.png",
-    },
-    {
-      id: 4,
-      title: "unity4",
-      text: "The forth",
-      src: "images/The Imaginarium (2).png",
-    },
-    {
-      id: 5,
-      title: "unity5",
-      text: "The fifth",
-      src: "images/The Imaginarium (2).png",
-    },
-  ];
-
   return (
     <Container>
       <Row>
-        {data.map((item) => (
+        {UnityListData.map((item) => (
           <Col key={item.id} xs={7} md={6} lg={5} xl={4} xxl={3}>
             <UnityItem
               src={item.src}
